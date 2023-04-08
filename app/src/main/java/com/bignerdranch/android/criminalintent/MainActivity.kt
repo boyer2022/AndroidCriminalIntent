1bf89b0cd4728f281e6f3a7e02a25c1358e5d44a
matt <emal>
2023-04-08 09:35:51 -0500

    Finished chapter 8 added crime_title_label to text field for Label

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    // pg 280 defines operation
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            // support is using the Jetpack library
+                // .add creates and commits a fragment transaction
+            // .beginTransaction() means "Create a new fragment transaction, include one
+            // add operation in it and then commit it."
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
     }
 }
